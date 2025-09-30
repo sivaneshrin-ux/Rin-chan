d1013dac310183bcfba1138260e4b8282e3cf981
sivaneshrin-ux <sivaneshrin@gmail.com>
2025-09-30 08:18:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['24160099'])
+API_ID = int(environ[''])
 API_HASH = environ['381515f2540b0bf817a68af8b352f5b7']
 BOT_TOKEN = environ['8026870399:AAH5eVx9RBQuTVW_ucDQ2WffEOi1eYT774U']
 
